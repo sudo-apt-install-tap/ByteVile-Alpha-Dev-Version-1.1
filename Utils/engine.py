59a75cb78d4f030053cfa3c1327fcc7f783efcac
lonedevx1 <121847306+sudo-apt-install-tap@users.noreply.github.com>
2025-10-06 15:47:43 +0530

    Update engine.py

--- Utils/engine.py
+++ Utils/engine.py
@@ -1,12 +1,3 @@
-import time
-import sys
-
-def slow_print(text, delay=0.03):
-    for char in text:
-        print(char, end="", flush=True)
-        time.sleep(delay)
-    print()
-
 def start_game():
     print("\n[Booting story engine...]\n")
     print("You wake up in a broken system. The air hums with static.")
