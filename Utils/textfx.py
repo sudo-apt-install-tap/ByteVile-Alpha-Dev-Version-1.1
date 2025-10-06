c3fa386f3e79c6cf36c5282f415b14a5feb4e4b6
lonedevx1 <121847306+sudo-apt-install-tap@users.noreply.github.com>
2025-10-06 15:48:07 +0530

    Update textfx.py

--- Utils/textfx.py
+++ Utils/textfx.py
@@ -1,5 +1,13 @@
 import os
 import platform
+import time
+import sys
+
+def slow_print(text, delay=0.03):
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+    print()
 
 def clear():
     os.system("cls" if platform.system() == "Windows" else "clear")
