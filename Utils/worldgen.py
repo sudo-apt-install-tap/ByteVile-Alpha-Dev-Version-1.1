f4554f46a0ca32222171dbc2f5cda4152addee7d
Lone-dev-x <tapishnud@gmail.com>
2025-12-22 21:45:40 +0530

    Made a random rng gen

--- Utils/worldgen.py
+++ Utils/worldgen.py
@@ -2,5 +2,9 @@ worldseed=1337
 
 import random
 
-def generate_room(x,y):
-    pass
+def _rng(x,y):
+    return random.Random(worldseed + x * 9999 + y * 8888)
+
+def worldgen():
+    r=_rng
+    return r
